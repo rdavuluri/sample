fffc9871fe33eec3a9cce099d7f37534c7299542
rdavuluri <rahuldavuluri@gmail.com>
2024-12-07 19:43:53 -0500

    Create auth.ts

---
+++ src/types/auth.ts
@@ -0,0 +1,18 @@
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterCredentials extends LoginCredentials {
+  name: string;
+}
+
+export interface ResetPasswordCredentials {
+  email: string;
+}
